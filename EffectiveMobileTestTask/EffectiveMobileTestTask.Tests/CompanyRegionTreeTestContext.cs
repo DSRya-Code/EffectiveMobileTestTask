d91218bdf47a6c7262ac5deb2efcabcad4c94303
D.Ryabov <ryabchikk@mail.ru>
2025-09-29 18:35:03 +0300

    тесты на 1 вариант
    Signed-off-by: D.Ryabov <ryabchikk@mail.ru>

---
+++ EffectiveMobileTestTask/EffectiveMobileTestTask.Tests/CompanyRegionTreeTestContext.cs
@@ -0,0 +1,51 @@
+﻿using EffectiveMobileTestTask.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectiveMobileTestTask.Tests
+{
+    public class CompanyRegionTreeTestContext
+    {
+        public CompanyRegionTree Tree { get; }
+
+        public CompanyRegionTreeTestContext()
+        {
+            var companies = CompanySetUp1();
+            Tree = new CompanyRegionTree();
+
+            foreach (var company in companies)
+            {
+                Tree.AddCompany(company);
+            }
+        }
+
+        public static List<Company> CompanySetUp1()
+        {
+            var company1 = new Company { Name = "Яндекс.Директ" };
+            company1.Paths.Add("/ru");
+
+            var company2 = new Company { Name = "Ревдинский рабочий" };
+            company2.Paths.Add("/ru/svrd/revda");
+            company2.Paths.Add("/ru/svrd/pervik");
+
+            var company3 = new Company { Name = "Газета уральских москвичей" };
+            company3.Paths.Add("/ru/msk");
+            company3.Paths.Add("/ru/permobl");
+            company3.Paths.Add("/ru/chelobl");
+
+            var company4 = new Company { Name = "Крутая реклама" };
+            company4.Paths.Add("/ru/svrd");
+
+            var result = new List<Company>();
+            result.Add(company1);
+            result.Add(company2);
+            result.Add(company3);
+            result.Add(company4);
+
+            return result;
+        }
+    }
+}
---
+++ EffectiveMobileTestTask/EffectiveMobileTestTask.Tests/CompanyRegionTreeTests.cs
@@ -0,0 +1,63 @@
+﻿using EffectiveMobileTestTask.BLL;
+
+namespace EffectiveMobileTestTask.Tests
+{
+    public class CompanyRegionTreeTests : IClassFixture<CompanyRegionTreeTestContext>
+    {
+
+        private readonly CompanyRegionTreeTestContext _context;
+
+        public CompanyRegionTreeTests(CompanyRegionTreeTestContext context)
+        {
+            _context = context;
+        }
+
+        [Fact]
+        public void TestFindCompaniesByExactPath()
+        {
+            var result = _context.Tree.FindCompaniesByPath("/ru/msk");
+
+            Assert.Equal(1, result.Count);
+            Assert.Contains(result, c => c.Name == "Газета уральских москвичей");
+        }
+
+        [Fact]
+        public void TestFindCompaniesByParentPath()
+        {
+            var result = _context.Tree.FindCompaniesByPath("/ru");
+
+            Assert.Equal(4, result.Count); // Все компании находятся под /ru
+            Assert.Contains(result, c => c.Name == "Яндекс.Директ");
+            Assert.Contains(result, c => c.Name == "Ревдинский рабочий");
+            Assert.Contains(result, c => c.Name == "Газета уральских москвичей");
+            Assert.Contains(result, c => c.Name == "Крутая реклама");
+        }
+
+        [Fact]
+        public void TestFindCompaniesByRegionPath()
+        {
+            var result = _context.Tree.FindCompaniesByPath("/ru/svrd");
+
+            Assert.Equal(2, result.Count); // Ревдинский рабочий и Крутая реклама
+            Assert.Contains(result, c => c.Name == "Ревдинский рабочий");
+            Assert.Contains(result, c => c.Name == "Крутая реклама");
+        }
+
+        [Fact]
+        public void TestFindCompaniesByCityPath()
+        {
+            var result = _context.Tree.FindCompaniesByPath("/ru/svrd/revda");
+
+            Assert.Single(result);
+            Assert.Contains(result, c => c.Name == "Ревдинский рабочий");
+        }
+
+        [Fact]
+        public void TestFindCompaniesByNonExistentPath()
+        {
+            var result = _context.Tree.FindCompaniesByPath("/ru/tatarstan/kazan");
+
+            Assert.Empty(result);
+        }
+    }
+}
