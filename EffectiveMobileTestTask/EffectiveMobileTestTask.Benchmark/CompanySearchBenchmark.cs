c122ca4db23a0c5dcff5d5370b091988648c4e85
D.Ryabov <ryabchikk@mail.ru>
2025-09-30 23:15:42 +0300

    расширение бенчмарка после исправлений поиска по дереву
    Signed-off-by: D.Ryabov <ryabchikk@mail.ru>

--- EffectiveMobileTestTask/EffectiveMobileTestTask.Benchmark/CompanySearchBenchmark.cs
+++ EffectiveMobileTestTask/EffectiveMobileTestTask.Benchmark/CompanySearchBenchmark.cs
@@ -19,7 +19,7 @@ namespace EffectiveMobileTestTask.Benchmark
         public void Setup()
         {
             // Создаём тестовые данные
-            var companies = GetTestCompanies();
+            var companies = GenerateLargeTestData();
 
             // Инициализируем дерево
             _tree = new CompanyRegionTree();
@@ -33,29 +33,61 @@ namespace EffectiveMobileTestTask.Benchmark
             _controller.BuildIndex(); // строим индекс
         }
 
+        // === Короткие пути ===
         [Benchmark]
-        public List<Company> SearchViaTree()
+        public List<Company> SearchViaTree_ShortPath()
         {
             return _tree.FindCompaniesByPath("/ru");
         }
 
         [Benchmark]
-        public async Task<IReadOnlyCollection<Company>> SearchViaController()
+        public async Task<IReadOnlyCollection<Company>> SearchViaController_ShortPath()
         {
             return await _controller.FindCompaniesByPath("/ru");
         }
 
-        private static List<Company> GetTestCompanies()
+        // === Средние пути ===
+        [Benchmark]
+        public List<Company> SearchViaTree_MediumPath()
+        {
+            return _tree.FindCompaniesByPath("/ru/msk/50");
+        }
+
+        [Benchmark]
+        public async Task<IReadOnlyCollection<Company>> SearchViaController_MediumPath()
+        {
+            return await _controller.FindCompaniesByPath("/ru/msk/50");
+        }
+
+        // === Длинные пути ===
+        [Benchmark]
+        public List<Company> SearchViaTree_LongPath()
+        {
+            return _tree.FindCompaniesByPath("/ru/msk/99/district/7/building/2");
+        }
+
+        [Benchmark]
+        public async Task<IReadOnlyCollection<Company>> SearchViaController_LongPath()
+        {
+            return await _controller.FindCompaniesByPath("/ru/msk/99/district/7/building/2");
+        }
+
+        private static List<Company> GenerateLargeTestData()
         {
             var companies = new List<Company>();
 
-            // Добавим больше данных для реалистичного теста
+            // 1000 компаний
             for (int i = 0; i < 1000; i++)
             {
                 var company = new Company { Name = $"Company {i}" };
-                company.Paths.Add($"/ru/msk/{i}");
-                company.Paths.Add($"/ru/spb/{i}");
-                company.Paths.Add($"/ru/svrd/{i}");
+
+                // Добавляем пути разной длины: от коротких до очень длинных
+                company.Paths.Add($"/ru/msk/{i % 100}/district/{i % 10}/building/{i % 5}");
+                company.Paths.Add($"/ru/spb/{i % 80}/area/{i % 5}");
+                company.Paths.Add($"/ru/svrd/{i % 60}/city/{i % 3}");
+                company.Paths.Add($"/ru/kzn/{i % 40}/street/{i % 2}");
+                company.Paths.Add($"/ru/{i % 10}"); // короткие пути
+
                 companies.Add(company);
             }
 
